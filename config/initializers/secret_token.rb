bf34f5d31d7c40c5aa1936601450c94ab603a8da
Jon Evans <jle@foraker.com>
2014-04-27 17:57:10 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oliverpictures::Application.config.secret_key_base = '68cd7ec249514a805eba4e869c6ed8e48a8d776bec63b267a3869c04aefcc585b9c4effd7edf4d506b66fec5bb665dc7cac3cbdbb2128496722d62d7cd3b0901'
